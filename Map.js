1592cd988d0ac43fc6d2bfa1a871ffe2342c746b
Someone Special <someone@devacademy.co.nz>
2016-01-24 12:42:29 +1300

    Add Map.js

---
+++ Map.js
@@ -0,0 +1,9 @@
+ function doubleAll(numbers) {
+      // SOLUTION GOES HERE
+var result = []
+var newResult = result.map(function(numbers){
+  return number *2;
+
+});
+}
+    module.exports = doubleAll
